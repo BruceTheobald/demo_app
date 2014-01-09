0b5a9d58fbf2a20ca974796cae3f4376d2b2b3b5
“Bruce <Bruce.Theobald@ma7.org>
2014-01-09 12:48:07 -0600

    Access-Control-Allow-Origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :allow_cross_domain_access
+  def allow_cross_domain_access
+    headers['Access-Control-Allow-Origin'] = '*'# http://localhost:9000
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
+    headers['Access-Control-Max-Age'] = '1728000'
+  end
 end
