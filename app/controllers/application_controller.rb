727fc1c99b8e3d8e505d9c49869a8b539edb36c8
“Bruce <Bruce.Theobald@ma7.org>
2014-01-09 12:53:09 -0600

    try specific address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :allow_cross_domain_access
   def allow_cross_domain_access
-    headers['Access-Control-Allow-Origin'] = '*'# http://localhost:9000
+#    headers['Access-Control-Allow-Origin'] = '*'# http://localhost:9000
+    headers['Access-Control-Allow-Origin'] = 'mranderso.dlinkddns.com' # http://localhost:9000
     headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
     headers['Access-Control-Max-Age'] = '1728000'
